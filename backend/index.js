9f716bbcc995b4224cd1b5f0e833710536e146b1
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2024-08-21 23:36:43 +1000

    Register API done

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 
-const mongoose = require("mongoose");
-
+// const mongoose = require("mongoose");
+require("./db/config");
 const app = express();
+const User = require("./db/User");
 
-const connectDB = async () => {
-  mongoose.connect("mongodb://localhost:27017/e-comm");
-  const productSchema = new mongoose.Schema({});
-  const product = mongoose.model("products", productSchema);
-  const data = await product.find();
-  console.warn(data);
-};
+app.use(express.json());
 
-connectDB();
+app.post("/register", async (req, resp) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  resp.send(result);
+});
+
+// const connectDB = async () => {
+//   mongoose.connect("mongodb://localhost:27017/e-comm");
+//   const productSchema = new mongoose.Schema({});
+//   const product = mongoose.model("products", productSchema);
+//   const data = await product.find();
+//   console.warn(data);
+// };
+
+// connectDB();
 
 app.listen(5000);
