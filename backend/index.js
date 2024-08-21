9b2717a3bab43370a296096801a7fb24455f130d
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2024-08-21 23:41:47 +1000

    CORS issue fixed

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 // const mongoose = require("mongoose");
 require("./db/config");
@@ -6,6 +7,7 @@ const app = express();
 const User = require("./db/User");
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
