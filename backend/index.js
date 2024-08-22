531492e37bc92e4b4c2239083170fe8cf84dec22
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2024-08-22 17:19:04 +1000

    Login API added

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,22 @@ app.use(cors());
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   resp.send(result);
+});
+
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User Found" });
+    }
+  } else {
+    resp.send({ result: "Enter Email or Password" });
+  }
 });
 
 // const connectDB = async () => {
