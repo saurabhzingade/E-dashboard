2c53168b9c4b9b1a49289b39f653124ef39c0d75
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2024-08-22 22:06:16 +1000

    Fetching Api dynamically

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Signup from "./components/Signup";
 import PrivateComponent from "./components/PrivateComponent";
 import Login from "./components/Login";
 import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Nav />
         <Routes>
           <Route element={<PrivateComponent />}>
-            <Route path="/" element={<h1>Product Component</h1>}></Route>
+            <Route path="/" element={<ProductList />}></Route>
             <Route path="/add" element={<AddProduct />}></Route>
             <Route path="/update" element={<h1>Update Component</h1>}></Route>
             <Route path="/logout" element={<h1>Logout Component</h1>}></Route>
