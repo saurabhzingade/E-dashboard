b5c7e8820f3474ce29bb1228baa561ba31ebc45a
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2024-08-22 16:40:41 +1000

    Routing Fixed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Signup from "./components/Signup";
+import PrivateComponent from "./components/PrivateComponent";
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path="/" element={<h1>Product Component</h1>}></Route>
-          <Route path="/add" element={<h1>Add Component</h1>}></Route>
-          <Route path="/update" element={<h1>Update Component</h1>}></Route>
-          <Route path="/logout" element={<h1>Logout Component</h1>}></Route>
-          <Route path="/profile" element={<h1>Profile Component</h1>}></Route>
+          <Route element={<PrivateComponent />}>
+            <Route path="/" element={<h1>Product Component</h1>}></Route>
+            <Route path="/add" element={<h1>Add Component</h1>}></Route>
+            <Route path="/update" element={<h1>Update Component</h1>}></Route>
+            <Route path="/logout" element={<h1>Logout Component</h1>}></Route>
+            <Route path="/profile" element={<h1>Profile Component</h1>}></Route>
+          </Route>
           <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
