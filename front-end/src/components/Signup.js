032e4d11d61d0e9676afb155753b4e18fc3fe05e
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2024-08-22 16:43:49 +1000

    If Logged in signup missing

--- front-end/src/components/Signup.js
+++ front-end/src/components/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
@@ -6,6 +6,12 @@ const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  }, []);
 
   const collectData = async () => {
     console.warn(name, email, password);
