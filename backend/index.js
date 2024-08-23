a958f424ecca0fe7a41c7fb7bdb30c5ede46fa17
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2024-08-23 18:21:07 +1000

    Search API added

--- backend/index.js
+++ backend/index.js
@@ -79,4 +79,21 @@ app.put("/product/:id", async (req, resp) => {
   resp.send(result);
 });
 
+app.get("/search/:key", async (req, resp) => {
+  let result = await Product.find({
+    $or: [
+      {
+        name: { $regex: req.params.key },
+      },
+      {
+        company: { $regex: req.params.key },
+      },
+      {
+        category: { $regex: req.params.key },
+      },
+    ],
+  });
+  resp.send(result);
+});
+
 app.listen(5000);
