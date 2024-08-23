702691de57b3d71732a50adccd267cdf126cce71
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2024-08-23 17:48:33 +1000

    Update API added

--- backend/index.js
+++ backend/index.js
@@ -71,4 +71,12 @@ app.get("/product/:id", async (req, resp) => {
   }
 });
 
+app.put("/product/:id", async (req, resp) => {
+  let result = await Product.updateOne(
+    { _id: req.params.id },
+    { $set: req.body }
+  );
+  resp.send(result);
+});
+
 app.listen(5000);
